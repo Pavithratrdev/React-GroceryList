44b72cd95d961e94926eab1fcdc8c372ceec65e0
pavithratrdev <pavithratr.mca@gmail.com>
2023-12-02 16:14:45 +0530

    React Proj

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = ({length}) => {
+    return (
+    <footer>
+        <p>
+           {length} List {length > 1 ? "Items" : "Item"}
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
